02bebd70ab589e9fe366e15ccf49fffe26246ca9
Christian Kotzbauer <christian.kotzbauer@gmail.com>
2020-07-18 16:51:39 +0200

    feat: expose globals too
    Signed-off-by: Christian Kotzbauer <christian.kotzbauer@gmail.com>

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -7,6 +7,7 @@ export default simpleTree;
 export type SimpleTree = Instance<"singleSelectDropdown" | "multiSelectDropdown" | "view">;
 export { Options, BaseOptions, TreeConfiguration } from "./types/options";
 export { TreeNode } from "./types/tree-node";
+export * from "./types/globals";
 export * from "./types/rects";
 export * from "./types/subscription";
 export * from "./ui/overlay-placement";
