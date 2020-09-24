cc4569dc7a67c327fc5bfa6e2d39627d376a3bff
Christian Kotzbauer <christian.kotzbauer@gmail.com>
2020-09-24 18:49:59 +0200

    doc: added more api-docs
    Signed-off-by: Christian Kotzbauer <christian.kotzbauer@gmail.com>

--- src/types/instance.ts
+++ src/types/instance.ts
@@ -8,17 +8,85 @@ export interface TreeModeNameMap {
     view: TreeNode | TreeNode[] | null;
 }
 
+/**
+ * Represents the public api of a tree-instance.
+ */
 export interface Instance<K extends keyof TreeModeNameMap> {
+    /**
+     * Applied configuration options for the current instance.
+     */
     options: BaseOptions;
+
+    /**
+     * Destroy the current instance and remove all modifications to the dom.
+     */
     destroy(): void;
+
+    /**
+     * Returns a tree-node which has the given `value` as value-property.
+     *
+     * @param value of a tree-node which should be returned.
+     * @returns a matching tree-node or null.
+     */
     getNode(value: string): TreeNode | null;
+
+    /**
+     * Moves the up or down in the same hierarchy-level.
+     *
+     * @param value tree-node object to move.
+     * @param direction of move-operation.
+     */
     moveNode(value: TreeNode | string, direction: "up" | "down"): void;
+
+    /**
+     * Returns one or multiple selected tree-node objects, depending on the mode.
+     *
+     * @returns a tree-node object in single-mode and view-mode without checkboxes. An array of nodes otherwise.
+     */
     getSelected(): TreeModeNameMap[K];
+
+    /**
+     * Resets the selection-state of tree-nodes to the given one(s).
+     *
+     * @param value a tree-node object in single-mode and view-mode without checkboxes. An array of nodes otherwise.
+     */
     setSelected(value: TreeModeNameMap[K]): void;
+
+    /**
+     * Changes the read-only state of the tree.
+     *
+     * @param readOnly state of the tree.
+     */
     setReadOnly(readOnly: boolean): void;
+
+    /**
+     * Only usable in single-mode. Shows an icon for a selected tree-node.
+     *
+     * @param cssClass to set on the icon-area.
+     */
     showEmphasizeIcon(cssClass: string): void;
+
+    /**
+     * Only usable in single-mode. Hides the emphasize-icon again.
+     */
     hideEmphasizeIcon(): void;
+
+    /**
+     * Add a listener to handle "selectionChanged" events.
+     *
+     * @param event "selectionChanged" event
+     * @param handler to execute custom logic on this event.
+     * @returns a subscription object to unsubscribe again.
+     */
     subscribe(event: "selectionChanged", handler: (d: TreeModeNameMap[K], e: string) => void): Subscription;
+
+    /**
+     * Add a listener to handle "selectionChanged" events. When the event is emitted for the first time, the subscription ends automatically.
+     *
+     * @param event "selectionChanged" event
+     * @param handler to execute custom logic on this event.
+     * @returns a subscription object to unsubscribe again.
+     */
     subscribeOnce(event: "selectionChanged", handler: (d: TreeModeNameMap[K], e: string) => void): Subscription;
 }
 
