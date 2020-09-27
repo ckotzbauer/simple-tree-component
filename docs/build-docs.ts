1bc98192c223b199d3d8820af4339181c5a7f32f
Christian Kotzbauer <christian.kotzbauer@gmail.com>
2020-09-27 10:58:57 +0200

    doc: fix typedoc-markdown plugin update
    Signed-off-by: Christian Kotzbauer <christian.kotzbauer@gmail.com>

--- docs/build-docs.ts
+++ docs/build-docs.ts
@@ -15,9 +15,9 @@ const ignoreFiles = ["_sidebar.md"];
     await Promise.all(mdFiles.map((f) => fs.promises.copyFile(`${docsDir}/${f}`, `${outDir}/${f}`)));
 
     // Merge Sidebar
-    const typesSidebar = (await fs.promises.readFile(`${outDir}/_sidebar.md`)).toString();
+    const typesSidebar = await generateTypesSidebar(outDir);
     const generalSidebar = (await fs.promises.readFile(`${docsDir}/_sidebar.md`)).toString();
-    const fullSidebar = generalSidebar + "\n" + typesSidebar.replace(/- \[Summary\]/g, "  - [Summary]");
+    const fullSidebar = generalSidebar + "\n" + typesSidebar;
 
     await fs.promises.writeFile(`${outDir}/_sidebar.md`, fullSidebar);
 
@@ -26,3 +26,21 @@ const ignoreFiles = ["_sidebar.md"];
 
     console.log("Documentation copied successfully!");
 })();
+
+async function generateTypesSidebar(outDir: string): Promise<string> {
+    const interfaces = (await fs.promises.readdir(`${outDir}/interfaces`)).filter((f) => !f.startsWith("_"));
+    const interfaceLines = interfaces.map(async (f) => `    - [${await extractTitle(outDir, f)}](interfaces/${f})`);
+    const lines = [
+        "- Types Documentation",
+        "  - [Summary](globals.md)",
+        "  - Interfaces",
+        ...(await Promise.all(interfaceLines)),
+    ];
+    return lines.join("\n");
+}
+
+async function extractTitle(outDir: string, file: string): Promise<string | undefined> {
+    const regex = /# Interface: (?<type>[a-zA-Z]*)/g;
+    const content = (await fs.promises.readFile(`${outDir}/interfaces/${file}`)).toString();
+    return regex.exec(content)?.groups?.type;
+}
