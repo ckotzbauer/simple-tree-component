4c3a2a5802c5528cd60ebe94344bc9a0c58027d3
Christian Kotzbauer <christian.kotzbauer@gmail.com>
2020-10-21 17:49:45 +0200

    chore: release 1.0.0-alpha.1
    Signed-off-by: Christian Kotzbauer <christian.kotzbauer@gmail.com>

--- dist/simple-tree-component.js
+++ dist/simple-tree-component.js
@@ -1,4 +1,4 @@
-/* simple-tree-component v0.0.1, @license MIT */
+/* simple-tree-component v1.0.0-alpha.1, @license MIT */
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
