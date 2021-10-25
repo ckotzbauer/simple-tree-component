cc07b474bb4906a3b9d4ce1eda4cee648e4bdf59
Christian Kotzbauer <christian.kotzbauer@gmail.com>
2021-10-25 09:26:02 +0200

    fix: also export InitTreeNode

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -5,6 +5,6 @@ declare const simpleTree: SimpleTreeFn;
 export default simpleTree;
 export { SimpleTree } from "./types/instance";
 export { Options, BaseOptions, TreeConfiguration } from "./types/options";
-export { TreeNode } from "./types/tree-node";
+export { TreeNode, InitTreeNode } from "./types/tree-node";
 export * from "./types/rects";
 export * from "./types/subscription";
